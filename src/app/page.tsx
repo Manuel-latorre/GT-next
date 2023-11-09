eefe0d478f72da4960cea2cdc1c63cf3e6ff3a31
Manuel-latorre <manuel.latorre11@gmail.com>
2023-11-09 12:09:15 -0300

    add faqs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import AboutUs from '@/components/AboutUs/AboutUs'
 import ContactoFaqs from '@/components/Faqs/ContactoFaqs'
 import Faqs from '@/components/Faqs/Faqs'
-
+import Footer from '@/components/Footer/Footer'
+import Servicios from '@/components/Servicios/Servicios'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
       <AboutUs/>
+      <Servicios/>
       <ContactoFaqs/>
       <Faqs/>
+      <Footer/>
     </div>
   )
 }
